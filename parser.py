8316f2abf647fb15fda0e46cb6183d7f03dfed9f
ItIsMrLaG <it.is.mr.lag@gmail.com>
2021-07-03 18:51:27 +0500

    Parser was wrapped in class + was created the reader

--- parser.py
+++ parser.py
@@ -1,56 +1,61 @@
 import requests as req
 from bs4 import BeautifulSoup as BeS
 
-city = input()
-city_url = {
-    'Москва': 'https://www.gismeteo.ru/weather-omsk-4578/',
-    'Санкт-Петербург': 'https://www.gismeteo.ru/weather-sankt-peterburg-4079/',
-     'Екатеринбург': 'https://www.gismeteo.ru/weather-yekaterinburg-4517/',
-     'Омск': 'https://www.gismeteo.ru/weather-omsk-4578/',
-     'Казань': 'https://www.gismeteo.ru/weather-kazan-4364/',
-     'Челябинск': 'https://www.gismeteo.ru/weather-chelyabinsk-4565/',
-     'Оренбург': 'https://www.gismeteo.ru/weather-orenburg-5159/',
-     'Кемерово': 'https://www.gismeteo.ru/weather-kemerovo-4693/',
-     'Сочи': 'https://www.gismeteo.ru/weather-sochi-5233/',
-     'Симферополь': 'https://www.gismeteo.ru/weather-simferopol-4995/',
-     'Ялта': 'https://www.gismeteo.ru/weather-yalta-5002/',
-     'Алушта': 'https://www.gismeteo.ru/weather-alushta-4996/',
-     'Владивосток': 'https://www.gismeteo.ru/weather-vladivostok-4877/',
-     'Анапа': 'https://www.gismeteo.ru/weather-anapa-5211/'
-}
 
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/91.0.4472.114 Safari/537.36'}
-params = None
+class FileReader:
+    def __init__(self, file=None):
+        self.file = file
+        self.write_f = open(file, 'a', encoding='utf8')
+        self.read_f = open(file, 'r', encoding='utf8')
 
+    def check_info(self):
+        lines = self.read_f.readlines()
+        cities = {}
+        for i in range(len(lines)):
+            lines[i] = lines[i].strip()
+            flag = lines[i].split(" -- ")
+            cities[flag[0]] = flag[1]
+        return cities
 
-def get_html(url):
-    info = req.get(url, headers=HEADERS, params=None)
-    return info
+    def minder(self, name, url):
+        file = self.write_f
+        print(f'{name} -- {url}', file=file)
+        self.write_f.close()
 
 
-def get_content(html):
-    info = {}
-    soup = BeS(html, 'html.parser')
-    item = soup.find('div', class_='forecast_frame forecast_now')
-    info['temp'] = item.find('span', class_='unit unit_temperature_c').get_text(strip=True)
-    info['full'] = item.find('span', class_='tip _top _center').get_text(strip=True)
-    info['wind'] = item.find('div', class_='nowinfo__value').get_text(strip=True) + ' м/с'
-    info['time'] = item.find('time').get_text(strip=True)
-    return info
+class Parser(FileReader):
+    def __init__(self, file, headers=None):
+        super().__init__(file)
+        if headers is None:
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                                     'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+        self.HEADERS = headers
+
+    def get_html(self, url):
+        info = req.get(url, headers=self.HEADERS, params=None)
+        return info
+
+    def get_content(self, html):
+        info = {}
+        soup = BeS(html, 'html.parser')
+        item = soup.find('div', class_='forecast_frame forecast_now')
+        info['temp'] = item.find('span', class_='unit unit_temperature_c').get_text(strip=True)
+        info['full'] = item.find('span', class_='tip _top _center').get_text(strip=True)
+        info['wind'] = item.find('div', class_='nowinfo__value').get_text(strip=True) + ' м/с'
+        info['time'] = item.find('time').get_text(strip=True)
+        return info
+
+    def parse(self, town):
+        html = self.get_html(self.choose_url(town))
+        if html.status_code == 200:
+            return self.get_content(html.text)
+        else:
+            print('ERROR')
+
+    def choose_url(self, town):
+        cities = self.check_info()
+        return cities[town] + 'now/'
 
 
-def parse():
-    html = get_html(URL)
-    if html.status_code == 200:
-        return get_content(html.text)
-    else:
-        print('ERROR')
-
-
-def changing(my_city):
-    url = city_url[my_city] + 'now/'
-    return url
-
-URL = changing(city)
-print(*parse())
+d = Parser('mind.txt')
+print(d.parse('Анапа'))
